13ef72b63cf24779ca5b2c007d4a2facd723f17f
Cvijander <cvija85@gmail.com>
2026-01-19 21:18:31 +0100

    match u pajtonu =  switch case u c#, js itd

---
+++ Lecture_1_Conditionals/house.py
@@ -0,0 +1,38 @@
+name = input ("What is your name? ")
+"""
+if name == "Harry":
+    print("Griffindor")
+elif name == "Hermione":
+    print("Griffindor")    
+elif name == "Ron":
+    print("Griffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")    
+
+"""
+
+"""
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Griffindor")
+elif name == "Draco":
+    print ("Slytherin") 
+else: 
+    print("Who? ")    
+"""
+
+match name:
+    case "Harry":
+        print("Griffindor")
+    case "Hermione":
+        print("Griffindor")
+    case "Ron":
+        print("Griffindor")
+    case "Draco":
+        print("Slytherin") 
+    case _:
+        print("Who?")      
+
+
+
