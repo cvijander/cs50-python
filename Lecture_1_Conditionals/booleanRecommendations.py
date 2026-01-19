06e7e46d8c70cc495146d740762869405f4c870d
Cvijander <cvija85@gmail.com>
2026-01-20 00:07:30 +0100

    Promimena and i pojednostavljenje samog upita

--- Lecture_1_Conditionals/booleanRecommendations.py
+++ Lecture_1_Conditionals/booleanRecommendations.py
@@ -1,31 +1,42 @@
 def main():
     difficulty = input ("Difficult or Casual? ")
     if not (difficulty == "Difficult" or difficulty == "Casual"):
-        ...
+        print("Enter  a valid difficulty")
+        return
 
 
     players = input("Multiplayer or Single-player? ")
-
-    if difficulty == "Difficult":
-        if players == "Multiplayer":
-            recommend("Poker")
-        elif players == "Simgle-player":
-            recommend("Klondike")          
-        else:
-            print("Enter a valid number of players")
+    if not ( players == "Multiplayer" or players == "Single-player"):
+        print("Enter a valid number of players")
+        return 
 
 
-    elif difficulty == "Casual":
-        if players == "Multiplayer":
-            recommend("Hearsts")
-        elif players == "Single-player":
-            recommend("Clock")  
-        else:
-            print("Enter a valid number of players")              
+    #if difficulty == "Difficult":
+     #   if players == "Multiplayer":
+      #      recommend("Poker")
+       # elif players == "Simgle-player":
+        #    recommend("Klondike")          
 
+    if difficulty == "Difficult" and players == "Multiplayer":
+         recommend("Poker") 
 
+    elif difficulty == "Difficult" and players =="Single-player":
+        recommend("Klondike")
+
+    #elif difficulty == "Casual":
+     #   if players == "Multiplayer":
+      #      recommend("Hearsts")
+       # elif players == "Single-player":
+        #    recommend("Clock")  
+
+    elif difficulty =="Casual" and players == "Multiplayer":
+        recommend("Hearsts")    
     else:
-            print("Enter  a valid difficulty")
+         recommend("Clock")    
+                  
+
+
+    
 
 def recommend(game):
     print ("You should play " , game)            
