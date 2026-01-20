60c70350d355f08faf32da644bc50652c823029a
Cvijander <cvija85@gmail.com>
2026-01-20 23:50:43 +0100

    User input in loops

---
+++ Lecture_2_Loops/trueLoops.py
@@ -0,0 +1,19 @@
+"""
+while True:
+    n = int (input("What is n? "))
+    if n < 0:
+         continue
+    else:
+         break
+
+
+"""
+
+while True:
+    n = int(input("What is n? "))
+    if n > 0:
+        break
+    
+      
+for _ in range(n):
+    print("meow")
