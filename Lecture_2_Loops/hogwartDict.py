4a9289c120462fda1e76fb3a79b0a916ddeb97c7
Cvijander <cvija85@gmail.com>
2026-01-22 14:28:38 +0100

    Dictionary with key and value and using for loop to get these values

--- Lecture_2_Loops/hogwartDict.py
+++ Lecture_2_Loops/hogwartDict.py
@@ -12,8 +12,23 @@ students = {
     "Draco" : "Slytherin"
 }
 
+# trazenje vrednosti rucno 
 print(students["Hermione"])
 print(students["Harry"])
 print(students["Ron"])
 print(students["Draco"])
+
+
+# trazenje vrednosti preko for petlje 
+
+for student in students:
+    print(student)
+
+
+# ovde dobijamo samo kljuceve tj prve vrednosti 
+
+# sa ovim dobijamo sledece vrednosti, dakle ovde kazemo da za svakog studenta uzmemo vrednosti 
+# ali kazemo ostampaj mi od dictionara jednuni tj dace mi tacno onu vrenost kkoja nam treba
+for student in students:
+    print(student, students[student] , sep =", ")
 
