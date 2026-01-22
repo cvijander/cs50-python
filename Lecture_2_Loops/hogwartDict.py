6cc5920b02096847f19ee6ca9fb520dea648eb31
Cvijander <cvija85@gmail.com>
2026-01-22 13:42:39 +0100

    Dictionary   dict on hogwart houses and students

---
+++ Lecture_2_Loops/hogwartDict.py
@@ -0,0 +1,19 @@
+#students = ["Hermione","Harry", "Ron","Draco"]
+#houses = ["Griffindor","Griffindor", "Griffindor","Slytherin"]
+
+
+emptyDictionary = {}
+
+
+students = {
+    "Hermione": "Griffindor",
+    "Harry" : "Griffindor",
+    "Ron" : "Griffindor",
+    "Draco" : "Slytherin"
+}
+
+print(students["Hermione"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
+
