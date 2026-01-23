23ae63ddb6684d590b18139ebffbf92857bc160c
Cvijander <cvija85@gmail.com>
2026-01-23 23:45:53 +0100

    Ovde je napravljena lista koja sadrzi kolekciju recnika, dok svaki recnik sadrzi podatke o osobi

--- Lecture_2_Loops/hogwartDictPatronus.py
+++ Lecture_2_Loops/hogwartDictPatronus.py
@@ -1,2 +1,12 @@
 # making a list of dictionaries, with 3 options name, house and patronus 
-students = {}
+students = [
+    {"name":"Hermione", "house": "Gryffindor","patronus": "Otter"},
+    {"name":"Harry","house":"Griffindor","patronus":"Stag"},
+    {"name":"Ron","house":"Griffindor","patronus":"Jack Russel terrier"},
+    {"name":"Draco","house":"Slytherin","patronus":None}
+
+]
+
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
