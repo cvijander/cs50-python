ab0d070eccb6bf947beefa081b4f9c649f1d21cf
Cvijander <cvija85@gmail.com>
2026-01-24 12:59:49 +0100

    Mario print column

---
+++ Lecture_2_Loops/mario.py
@@ -0,0 +1,27 @@
+"""
+print("#")
+print("#")
+print("#")
+"""
+
+"""
+
+for _ in range(3):
+    print("%")
+
+    """
+
+def main():
+    print_Column(3)
+
+
+
+
+def print_Column(height):
+    for _ in range(height):
+        print("#")
+
+
+
+
+main()
