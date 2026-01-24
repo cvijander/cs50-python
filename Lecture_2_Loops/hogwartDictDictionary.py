2c8a6551092cb311d0096c1d22be77b7b2fb1d6f
Cvijander <cvija85@gmail.com>
2026-01-24 02:34:12 +0100

    Dict in dicrionary - recnik u recniku

---
+++ Lecture_2_Loops/hogwartDictDictionary.py
@@ -0,0 +1,9 @@
+students = {
+    "Hermione": {"house":"Gryffindor","patronus":"Otter"},
+    "Harry" :   {"house":"Gryffindor","patronus": "Stag"},
+    "Ron":      {"house":"Gryffindor","patronus":"Jack russel"},
+    "Draco":    {"house": "Slytherin","patronus": None}
+}
+
+
+print (students["Hermione"] , students["Hermione"]["house"], students["Hermione"]["patronus"])
